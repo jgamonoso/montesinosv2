ff56cbfb8f28cc892b0f8308e3a5bf8be201bc2d
Juan Gamonoso <juan.gamonoso@hotmail.com>
2023-03-18 01:53:28 +0100

    login funcional revisar el ngModel y ngModelOptions=standalone: true

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,12 +8,35 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+
+  credentials = {
+    login: '',
+    password: ''
+  };
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   login() {
     console.log('submit');
-    this.router.navigateByUrl('/');
+    this.authService.authenticate(this.credentials).subscribe(
+      (response) => {
+        debugger
+        if (response.status === 'ok') {
+          // Navegar a la página principal (por ejemplo, DashboardComponent)
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          console.error('Error de inicio de sesión');
+        }
+      },
+      (error) => {
+        debugger
+        console.error('Error de inicio de sesión', error);
+      }
+    );
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from '../../environments/environment';
+
+const base_url = environment.base_url;
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {
+
+  constructor(
+    private http: HttpClient
+  ) {
     console.log('Hola, soy AuthService');
+  }
+
+  authenticate(credentials: { login: string; password: string }): Observable<any> {
+    debugger;
+    return this.http.post<any>(base_url + 'login/login.php', credentials);
   }
 }
