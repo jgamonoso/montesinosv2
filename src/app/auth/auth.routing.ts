a4d6eaed5fac8b48d448b80b7a9c52757391bfb2
Juan Gamonoso <juan.gamonoso@hotmail.com>
2023-03-20 03:27:11 +0100

    login routing fix

--- src/app/auth/auth.routing.ts
+++ src/app/auth/auth.routing.ts
@@ -2,13 +2,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ResetComponent } from './reset/reset.component';
 
 const routes: Routes = [
   {
     path: 'auth',
     children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
+      { path: 'reset', component: ResetComponent },
     ],
   },
 ];
