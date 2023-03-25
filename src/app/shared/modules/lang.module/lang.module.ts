aa12030a25243bc925b5062f3cad59e61eee9a0c
Juan Gamonoso <juan.gamonoso@hotmail.com>
2023-03-25 01:37:00 +0100

    fix LangModule

--- src/app/shared/modules/lang.module/lang.module.ts
+++ src/app/shared/modules/lang.module/lang.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { SelectLangComponent } from './component/select-lang.component/select-lang.component';
-import { NgxFormlyModule } from '../ngx-formly.module/ngx-formly.module';
-import {CoreModule} from '../../core.module';
 
 @NgModule({
   imports: [
-    CoreModule,
     TranslateModule,
-    NgxFormlyModule
   ],
   declarations: [
     SelectLangComponent
