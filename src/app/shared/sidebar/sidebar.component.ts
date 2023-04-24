eb362b5351a9588141ca94cdedf3a75bb3ed5eca
Juan Gamonoso <juan.gamonoso@hotmail.com>
2023-04-25 00:27:37 +0200

    onSidebarLinkClick solution

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -30,4 +30,19 @@ export class SidebarComponent implements OnInit {
     this.authService.removeStoredData();
     this.router.navigateByUrl('/');
   }
+
+  onSidebarLinkClick(): void {
+    // Compruebe si el menú está abierto en dispositivos móviles
+    if (window.innerWidth < 1170 && document.body.classList.contains('show-sidebar')) {
+      // Cierre el menú
+      document.body.classList.remove('show-sidebar');
+
+      // Cambie la clase 'ti-close' a 'ti-menu' en el botón del menú
+      const menuButton = document.querySelector('.nav-toggler i');
+      if (menuButton) {
+        menuButton.classList.remove('ti-close');
+        menuButton.classList.add('ti-menu');
+      }
+    }
+  }
 }
