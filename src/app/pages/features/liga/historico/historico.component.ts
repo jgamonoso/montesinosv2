391175cb5f59c85dab91ff4bb043d8cd0dac821f
Juan Gamonoso <juan.gamonoso@hotmail.com>
2023-06-02 00:57:43 +0200

    Palmares historico

--- src/app/pages/features/liga/historico/historico.component.ts
+++ src/app/pages/features/liga/historico/historico.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { LoadingService } from 'src/app/shared/modules/loading.module/service/loading.service';
+import { SharedService } from 'src/app/services/shared.service';
+import { LigaService } from '../liga.service';
 
 @Component({
   selector: 'app-historico',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoricoComponent implements OnInit {
 
-  constructor() { }
+  dataLoaded: boolean;
+  ligaGuardadaEnSesion: any;
+
+  listaPalmares: any[];
+  listaRecords: any[];
+
+  constructor(
+    private authService: AuthService,
+    private ligaService: LigaService,
+    private readonly loadingService: LoadingService,
+    private sharedService: SharedService,
+  ) { }
 
   ngOnInit(): void {
+    this.dataLoaded = false;
+    this.ligaGuardadaEnSesion = this.authService.getStoredLigaGuardada();
+    this.loadInitialData();
   }
 
+  loadInitialData() {
+    this.loadingService.setLoadingState(true);
+    forkJoin([
+      this.ligaService.obtenerPalmaresLiga(this.ligaGuardadaEnSesion.ligaVisible),
+      this.ligaService.obtenerRecordsLiga(this.ligaGuardadaEnSesion.ligaVisible),
+      this.sharedService.obtenerListaEquiposNombre(),
+    ]).subscribe(
+      ([palmaresLiga, recordsLiga, listaEquiposNombre]) => {
+        // agrupar palmares por temporada
+        this.listaPalmares = palmaresLiga.reduce((groupedPalmares, palmares) => {
+          const temporadaNombre = palmares.temporada.nombre;
+          if (!groupedPalmares[temporadaNombre]) {
+            groupedPalmares[temporadaNombre] = [];
+          }
+          groupedPalmares[temporadaNombre].push(palmares);
+          return groupedPalmares;
+        }, {});
+
+        this.listaRecords = recordsLiga;
+        this.dataLoaded = true;
+        this.loadingService.setLoadingState(false);
+      },
+      (error) => {
+        console.error('Error en carga de palmarés historico', error.message);
+        this.loadingService.setLoadingState(false);
+      }
+    );
+  }
+
+  objectKeys(obj) {
+    if (obj === null || obj === undefined) {
+      return [];
+    }
+    return Object.keys(obj);
+  }
 }
---
+++ src/app/pages/features/liga/liga.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { _API_ENDPOINTS } from 'src/app/services/api/api-settings';
+import { HttpParametersClass } from 'src/app/shared/modules/http.module/service/http-parameters.class';
+import { HttpService } from 'src/app/shared/modules/http.module/service/http.service';
+import { LoadingService } from 'src/app/shared/modules/loading.module/service/loading.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LigaService {
+
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly loadingService: LoadingService
+  ) { }
+
+  obtenerPalmaresLiga(pkLiga: number): Observable<any> {
+    const httpParametersClass = new HttpParametersClass({
+      url: `${_API_ENDPOINTS.host}${_API_ENDPOINTS.liga.start}`,
+      body: {
+        action: 'obtenerPalmaresLiga',
+        pkLiga: pkLiga
+      }
+    });
+    return this.httpService.post(httpParametersClass).pipe(
+      tap(
+        response => {
+          // Respuesta OK
+        },
+        error => {
+          this.loadingService.setLoadingState(false);
+        }
+      )
+    );
+  }
+
+  obtenerRecordsLiga(pkLiga: number): Observable<any> {
+    const httpParametersClass = new HttpParametersClass({
+      url: `${_API_ENDPOINTS.host}${_API_ENDPOINTS.liga.start}`,
+      body: {
+        action: 'obtenerRecordsLiga',
+        pkLiga: pkLiga
+      }
+    });
+    return this.httpService.post(httpParametersClass).pipe(
+      tap(
+        response => {
+          // Respuesta OK
+        },
+        error => {
+          this.loadingService.setLoadingState(false);
+        }
+      )
+    );
+  }
+
+  obtenerListaEquiposLiga(pkLiga: number): Observable<any> {
+    const httpParametersClass = new HttpParametersClass({
+      url: `${_API_ENDPOINTS.host}${_API_ENDPOINTS.liga.start}`,
+      body: {
+        action: 'obtenerListaEquiposLiga',
+        pkLiga: pkLiga
+      }
+    });
+    return this.httpService.post(httpParametersClass).pipe(
+      tap(
+        response => {
+          // Respuesta OK
+        },
+        error => {
+          this.loadingService.setLoadingState(false);
+        }
+      )
+    );
+  }
+}
